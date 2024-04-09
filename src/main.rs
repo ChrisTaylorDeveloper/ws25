0de74fbbf8e00de88d15a8ea233e466ef4edc6d7
Chris Taylor <chris@christaylordeveloper.co.uk>
2024-04-09 16:59:57 +0200

    Print out browser request.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io::BufRead;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = std::net::TcpListener::bind("127.0.0.1:9999").unwrap();
+    for mut stream in listener.incoming().flatten() {
+        let mut rdr = std::io::BufReader::new(&mut stream);
+        let mut l = String::new();
+        loop {
+            rdr.read_line(&mut l).unwrap();
+            print!("{l}");
+        }
+    }
 }
